d1608230dcffb682fac3d726d9704a3c8a72312a
Jim Barnett <james.barnett.jr@gmail.com>
2015-03-13 20:50:50 -0500

    Minor style change. Still learning the "android" way

--- app/src/main/java/com/va/dtcandroid/PiecesPageAdapter.java
+++ app/src/main/java/com/va/dtcandroid/PiecesPageAdapter.java
@@ -15,26 +15,26 @@ import android.widget.*;
  * Created by jbarnett on 3/13/15.
  */
 public class PiecesPageAdapter extends FragmentStatePagerAdapter {
-    private Collection _collection;
+    private Collection mCollection;
 
     public PiecesPageAdapter(FragmentManager fm, Collection collection) {
         super(fm);
-        _collection = collection;
+        mCollection = collection;
     }
 
     @Override
     public int getCount() {
-        return _collection.getPieces().size();
+        return mCollection.getPieces().size();
     }
 
     @Override
     public Fragment getItem(int position) {
-        return ArrayListFragment.newInstance(_collection, position);
+        return ArrayListFragment.newInstance(mCollection, position);
     }
 
     public static class ArrayListFragment extends ListFragment {
-        private int _index;
-        private Collection _collection;
+        private int mIndex;
+        private Collection mCollection;
 
         public static final String SELECTED_COLLECTION = "selectedCollection";
         public static final String INDEX = "index";
@@ -63,7 +63,7 @@ public class PiecesPageAdapter extends FragmentStatePagerAdapter {
             extractArgs(savedInstanceState);
             View v = inflater.inflate(R.layout.fragment_piece, container, false);
             TextView tv = (TextView) v.findViewById(R.id.pieceTitleText);
-            tv.setText("Fragment #" + _index);
+            tv.setText("Fragment #" + mIndex);
 
             return v;
         }
@@ -74,8 +74,8 @@ public class PiecesPageAdapter extends FragmentStatePagerAdapter {
             extractArgs(savedInstanceState);
             List<String> list = new ArrayList<String>();
 
-            if (_collection != null) {
-                for (Piece piece : _collection.getPieces()) {
+            if (mCollection != null) {
+                for (Piece piece : mCollection.getPieces()) {
                     list.add(piece.getTitle());
                 }
             }
@@ -86,8 +86,8 @@ public class PiecesPageAdapter extends FragmentStatePagerAdapter {
 
         private void extractArgs(Bundle bundle) {
             if (bundle != null) {
-                _collection = bundle.getParcelable(SELECTED_COLLECTION);
-                _index = bundle.getInt(INDEX);
+                mCollection = bundle.getParcelable(SELECTED_COLLECTION);
+                mIndex = bundle.getInt(INDEX);
             }
         }
     }
